cd37e987c3d2becd30359d12cb9e0a30d21f05b4
robin <zhangbinatp@gmail.com>
2018-05-22 17:24:50 +0800

    add_repository_of_fast-neural-style-tensorflow
    Signed-off-by: robin <zhangbinatp@gmail.com>

---
+++ fast-neural-style-tensorflow/eval.py
@@ -0,0 +1,86 @@
+# coding: utf-8
+from __future__ import print_function
+import tensorflow as tf
+from preprocessing import preprocessing_factory
+import reader
+import model
+import time
+import os
+import matplotlib.pyplot as plt
+
+tf.app.flags.DEFINE_string('loss_model', 'vgg_16', 'The name of the architecture to evaluate. '
+                           'You can view all the support models in nets/nets_factory.py')
+tf.app.flags.DEFINE_integer('image_size', 256, 'Image size to train.')
+tf.app.flags.DEFINE_string("model_file", "models/mosaic/fast-style-model.ckpt-35000", "an image transformation network")
+tf.app.flags.DEFINE_string("image_file", "img/test.jpg", "the test mage")
+
+FLAGS = tf.app.flags.FLAGS
+
+
+def main(_):
+
+    # Get image's height and width.
+    height = 0
+    width = 0
+    with tf.gfile.GFile(FLAGS.image_file, 'rb') as f:
+        with tf.Session().as_default() as sess:
+            if FLAGS.image_file.lower().endswith('png'):
+                image = sess.run(tf.image.decode_png(f.read()))
+            else:
+                image = sess.run(tf.image.decode_jpeg(f.read()))
+            height = image.shape[0]
+            width = image.shape[1]
+    tf.logging.info('Image size: %dx%d' % (width, height))
+
+    with tf.Graph().as_default():
+        with tf.Session().as_default() as sess:
+
+            # Read image data.
+            image_preprocessing_fn, _ = preprocessing_factory.get_preprocessing(
+                FLAGS.loss_model,
+                is_training=False)
+            image = reader.get_image(FLAGS.image_file, height, width, image_preprocessing_fn)
+            print(image)
+            plt.subplot(121)
+            plt.imshow(sess.run(image))
+
+            # Add batch dimension
+            image = tf.expand_dims(image, 0)
+
+            generated = model.net(image, training=False)
+            generated = tf.cast(generated, tf.uint8)
+
+
+            # Remove batch dimension
+            generated = tf.squeeze(generated, [0])
+
+            # Restore model variables.
+            saver = tf.train.Saver(tf.global_variables(), write_version=tf.train.SaverDef.V1)
+            sess.run([tf.global_variables_initializer(), tf.local_variables_initializer()])
+            # Use absolute path
+            FLAGS.model_file = os.path.abspath(FLAGS.model_file)
+            saver.restore(sess, FLAGS.model_file)
+            
+            summary_writer = tf.summary.FileWriter("logs",sess.graph)
+
+            # Make sure 'generated' directory exists.
+            generated_file = 'generated/res.jpg'
+            if os.path.exists('generated') is False:
+                os.makedirs('generated')
+
+            # Generate and write image data to file.
+            with tf.gfile.GFile(generated_file, 'wb') as f:
+                plt.subplot(122)
+                plt.imshow(sess.run(generated))
+                plt.show()
+                start_time = time.time()
+                f.write(sess.run(tf.image.encode_jpeg(generated)))
+                end_time = time.time()
+                tf.logging.info('Elapsed time: %fs' % (end_time - start_time))
+
+                tf.logging.info('Done. Please check %s.' % generated_file)
+
+
+if __name__ == '__main__':
+    tf.logging.set_verbosity(tf.logging.INFO)
+    tf.app.run()
