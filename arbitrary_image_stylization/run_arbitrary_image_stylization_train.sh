0f55312c2c8d3d313b9404663838a153db8c7dd6
robin <zhangbinatp@gmail.com>
2018-05-22 18:06:58 +0800

    add_repository_of_arbitrary_image_stylization
    Signed-off-by: robin <zhangbinatp@gmail.com>

---
+++ arbitrary_image_stylization/run_arbitrary_image_stylization_train.sh
@@ -0,0 +1,28 @@
+echo "input create or train :"
+read a
+echo "input is $a"
+
+STYLE_IMAGES_PATHS=./images/style_images/*.jpg
+RECORDIO_PATH=./tmp/tfrecord/style_images.tfrecord
+if [ $a = create ] ; then
+python image_stylization_create_dataset.py \
+    --style_files=$STYLE_IMAGES_PATHS \
+    --output_file=$RECORDIO_PATH \
+    --compute_gram_matrices=False \
+    --logtostderr
+fi
+
+if [ $a = train ] ; then
+python arbitrary_image_stylization_train.py \
+      --batch_size=8 \
+      --imagenet_data_dir=./tmp/imagenet-2012-tfrecord \
+      --vgg_checkpoint=./tmp/pretrain/vgg_16.ckpt \
+      --inception_v3_checkpoint=./tmp/pretrain/inception_v3.ckpt\
+      --style_dataset_file=$RECORDIO_PATH \
+      --train_dir=./tmp/train_dir \
+      --content_weights={\"vgg_16/conv3\":2.0} \
+      --random_style_image_size=False \
+      --augment_style_images=False \
+      --center_crop=True \
+      --logtostderr
+fi
---
+++ arbitrary_image_stylization/run_arbitrary_image_stylization_with_weights.sh
@@ -0,0 +1,10 @@
+python arbitrary_image_stylization_with_weights.py \
+  --checkpoint=./tmp/train_dir/\
+  --output_dir=./images/output/ \
+  --style_images_paths=./images/style_images/*.jpg \
+  --content_images_paths=./images/content_images/*.jpg \
+  --image_size=256 \
+  --content_square_crop=False \
+  --style_image_size=256 \
+  --style_square_crop=False \
+  --logtostderr
